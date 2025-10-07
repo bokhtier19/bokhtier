86707b2b6f1ec0c26556a6f9abc5ea0b683c36d6
bokhtier19 <bokhtierelius19@gmail.com>
2025-10-07 12:35:15 +0530

    adding google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import ScrollNav from "../../components/ScrollNav";
 import { AnimatePresence } from "framer-motion";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 const space = Space_Mono({
     subsets: ["latin"],
@@ -41,6 +42,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                         <SpeedInsights />
                     </ThemeProvider>
                 </AnimatePresence>
+                <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`} />
+                <Script id="google-analytics">
+                    {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+              `}
+                </Script>
             </body>
         </html>
     );
